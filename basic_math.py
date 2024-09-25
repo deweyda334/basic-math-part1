7268430a95e800178b07950457801e5a14023731
deweyda334 <153093259+deweyda334@users.noreply.github.com>
2024-09-25 13:25:51 -0400

    Add files via upload

---
+++ basic_math.py
@@ -0,0 +1,47 @@
+#Dax Dewey
+#basic_math
+#9/25/24
+
+
+num1 = 56
+num2 = 90
+num3 = 89
+num4 = 36
+num5 = 210
+num6 = 14
+num7 = 34
+num8 = 634
+
+
+
+
+
+
+
+product = num1 * num2
+
+print(f'{num1} * {num2} = {product}')
+
+sum = num3 + num4
+
+print(f'{num3} + {num4} = {sum}')
+
+difference = num5 - num6
+
+print(f'{num5} - {num6} = {difference}')
+
+quotient = num7 / num8
+
+print(f'{num7} / {num8} = {quotient}')
+
+number_cubed = num1 ** 3
+
+print(f'{num1} raised to the power of 3 = {number_cubed}')
+
+
+
+
+
+
+
+
